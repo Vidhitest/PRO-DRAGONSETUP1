cc2ec5105a3cf02752418ce426df35d100a10dc4
【★༒✞︎Drak-ℓєɠεɳ∂-Pro】 <98252847+DARK-LEGEND-PRO@users.noreply.github.com>
2022-02-25 06:29:32 -0800

    Update updater.py

--- setup/updater.py
+++ setup/updater.py
@@ -43,7 +43,7 @@ async def update_requirements(main, test):
     try:
         for i in a:
             await runcmd(f"pip install {i}")
-            print(f"⚡- Installing -⚡ {i}")
+            print(f"⚡💥- Drak-Legend-Pro-💥⚡ {i}")
     except Exception as e:
         print(f"Error while installing requirments {str(e)}")
 
